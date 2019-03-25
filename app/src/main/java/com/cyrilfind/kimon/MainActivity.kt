b01a938b08f80a4f2e386148f57d4b0a301c3657
Cyril Findeling <cyril@edupad.com>
2019-03-25 19:04:08 +0100

    android studio empty activity init

---
+++ app/src/main/java/com/cyrilfind/kimon/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cyrilfind.kimon
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
